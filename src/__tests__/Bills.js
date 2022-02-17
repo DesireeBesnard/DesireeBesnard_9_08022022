6c196d3caeaf420bb4f3ffdb591a3249d4d8c497
DesireeBesnard <desiree.besnard@gmail.com>
2022-02-17 14:19:28 +0100

    test when eye icon clicked

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,14 +5,14 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import Bills from "../containers/Bills"
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store"
-
 import router from "../app/Router.js";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("../app/store", () => mockStore)
-
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -46,12 +46,40 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
       expect(windowIcon.classList.contains("active-icon")).toBeTruthy()
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+
+    describe("When I click on the eye icon", () => {
+      test("Then the modal should open up", () => {
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+
+        document.body.innerHTML = BillsUI({ data: bills })
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const store = null
+        const billsContainer = new Bills({ document, onNavigate, store, localStorage: window.localStorage })
+
+        const handleClickIconEye = jest.fn(billsContainer.handleClickIconEye)
+        const eyes = screen.getAllByTestId('icon-eye')
+        for (const eye of eyes) {
+          eye.addEventListener('click', handleClickIconEye(eye))
+        }
+        userEvent.click(eyes[0])
+        expect(handleClickIconEye).toHaveBeenCalled()
+
+        const modale = screen.getByText('Justificatif')
+        expect(modale).toBeTruthy()
+      })
     })
   })
 })
