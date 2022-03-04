f638a3376133fb9c50865b545e99922c18e9e601
DesireeBesnard <desiree.besnard@gmail.com>
2022-03-04 20:06:31 +0100

    newBill tests

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -247,37 +247,4 @@ describe("Given I am connected as an employee", () => {
       })
     })
   })
-
-  describe("When an error occurs on API", () => {
-
-    beforeEach(() => {
-      jest.spyOn(mockStore, "bills")
-      Object.defineProperty(
-          window,
-          'localStorage',
-          { value: localStorageMock }
-      )
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee',
-        email: "e@e"
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.appendChild(root)
-      router()
-    })
-
-    test("Then in case of 404 error a 404 error message should appear", async () => {
-      mockStore.bills.mockImplementationOnce(() => {
-        return {
-          list : () =>  {
-            return Promise.reject(new Error("Erreur 404"))
-          }
-        }})
-      window.onNavigate(ROUTES_PATH.NewBill)
-      await new Promise(process.nextTick);
-      const message = await screen.getByText("Mes notes de frais")
-      expect(message).toBeTruthy()
-    })
-  })
 })
